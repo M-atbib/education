b59f9763c689562f19888348a181d3cbc4307c76
Mokhtar Atbib <94636437+M-atbib@users.noreply.github.com>
2023-05-05 01:41:24 +0200

    SideBar good
    ila chti had comment checki m3ak hir click dial close dial sidebar fmobile

--- resources/views/pages/admin/layouts/_sidebar.blade.php
+++ resources/views/pages/admin/layouts/_sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="sidebar">
-   @if (Auth::user()->type_user == 1)
-        @include('pages.admin.layouts.sidebar._student')
-   @elseif (Auth::user()->type_user == 2)
-        @include('pages.admin.layouts.sidebar._teacher')
-   @else
-        @include('pages.admin.layouts.sidebar._superadmin')
-   @endif
+     @if (Auth::user()->type_user == 1)
+     @include('pages.admin.layouts.sidebar._student')
+     @elseif (Auth::user()->type_user == 2)
+     @include('pages.admin.layouts.sidebar._teacher')
+     @else
+     @include('pages.admin.layouts.sidebar._superadmin')
+     @endif
 </div>
--- resources/views/pages/admin/layouts/sidebar/_student.blade.php
+++ resources/views/pages/admin/layouts/sidebar/_student.blade.php
@@ -1,26 +1,52 @@
-<ul>
-    <a href="#">
-        <i class="fa-solid fa-graduation-cap"></i>
-        Apprentissage
-    </a>
-    <a href="#">
-       <i class="fa-solid fa-comments"></i>
-        Group chat
-    </a>
-    <a href="#">
-        <i class="fa-solid fa-comment"></i>
-        Chat prof
-    </a>
-    <a href="#">
-        <i class="fa-solid fa-credit-card"></i>
-        Paiments
-    </a>
-    <a href="#">
-        <i class="fa-solid fa-certificate"></i>
-        Attestation
-    </a>
-    <a href="#">
-        <i class="fa-solid fa-calendar-check"></i>
-        Emploi du temps
-    </a>
-</ul>
+<div class="sidebar_container" id="dropdown">
+    <div class="sidebar_container-dropdown">
+        <i class="fa-solid fa-caret-down"></i>
+        <p>Sidebar</p>
+    </div>
+
+    <div class="sidebar_container-content" id="content">
+        <i class="fa-solid fa-xmark" id="close"></i>
+        <ul>
+            <a href=" #">
+                <i class="fa-solid fa-graduation-cap"></i>
+                Apprentissage
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-comments"></i>
+                Group chat
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-comment"></i>
+                Chat prof
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-credit-card"></i>
+                Paiments
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-certificate"></i>
+                Attestation
+            </a>
+            <a href="#">
+                <i class="fa-solid fa-calendar-check"></i>
+                Emploi du temps
+            </a>
+        </ul>
+    </div>
+</div>
+
+<script language="javascript">
+    document.addEventListener('DOMContentLoaded', () => {
+        const dropdown = document.getElementById('dropdown');
+        const content = document.getElementById('content');
+        const close = document.getElementById('close');
+
+        dropdown.addEventListener('click', () => {
+            content.style.display = 'block';
+        })
+
+        close.addEventListener('click', () => {
+            content.style.display = "none";
+        })
+    })
+</script>
