ffe7681994ecdab8fe779a638d191edfc4e2591e
harabazan zouhir <zouhirharabazann@gmail.com>
2023-05-11 12:26:03 +0100

    footer

--- resources/views/includes/_footer.blade.php
+++ resources/views/includes/_footer.blade.php
@@ -1,7 +1,7 @@
 
 <script src="https://kit.fontawesome.com/02c1184f7f.js" crossorigin="anonymous"></script>
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"></script> --}}
-<script src="{{ asset('assets/visitor/js/main.js') }}"></script>
+<script src="{{ asset('assets/pages/js/main.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
