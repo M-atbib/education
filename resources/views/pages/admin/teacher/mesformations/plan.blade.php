09d34c2cae9eabcc4171fefaa4742a6aa85e03b8
Mokhtar Atbib <94636437+M-atbib@users.noreply.github.com>
2023-07-10 19:06:14 +0100

    retouches

--- resources/views/pages/admin/teacher/mesformations/plan.blade.php
+++ resources/views/pages/admin/teacher/mesformations/plan.blade.php
@@ -61,6 +61,22 @@
                     </div>
                     <p>1h30m</p>
                 </div>
+                <div class="item">
+                    <div class="left">
+                        <i class="fa-solid fa-bars"></i>
+                        <p>02</p>
+                        <h2>Plan de temps</h2>
+                    </div>
+                    <p>1h30m</p>
+                </div>
+                <div class="item">
+                    <div class="left">
+                        <i class="fa-solid fa-bars"></i>
+                        <p>02</p>
+                        <h2>Plan de temps</h2>
+                    </div>
+                    <p>1h30m</p>
+                </div>
 
                 <div class="item">
                     <div class="left">
@@ -86,48 +102,6 @@
         <h1>Groups</h1>
 
         <div class="formations-plan_groups-list">
-            <a href="{{ route('mesformations.class') }}" class="formations-plan_groups-list_item">
-                <div class="title">
-                    <h2>Class 08</h2>
-                    <i class="fa-solid fa-chevron-right"></i>
-                </div>
-
-                <div class="profiles">
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/27.jpg" width="200" />
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/38.jpg" width="200" />
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/50.jpg" width="200" />
-                    <div class="profiles-more">+09</div>
-                </div>
-            </a>
-
-            <a href="{{ route('mesformations.class') }}" class="formations-plan_groups-list_item">
-                <div class="title">
-                    <h2>Class 08</h2>
-                    <i class="fa-solid fa-chevron-right"></i>
-                </div>
-
-                <div class="profiles">
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/27.jpg" width="200" />
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/38.jpg" width="200" />
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/50.jpg" width="200" />
-                    <div class="profiles-more">+09</div>
-                </div>
-            </a>
-
-            <a href="{{ route('mesformations.class') }}" class="formations-plan_groups-list_item">
-                <div class="title">
-                    <h2>Class 08</h2>
-                    <i class="fa-solid fa-chevron-right"></i>
-                </div>
-
-                <div class="profiles">
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/27.jpg" width="200" />
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/38.jpg" width="200" />
-                    <img src="http://127.0.0.1:8000/uploads/teatcher/50.jpg" width="200" />
-                    <div class="profiles-more">+09</div>
-                </div>
-            </a>
-
             <a href="{{ route('mesformations.class') }}" class="formations-plan_groups-list_item">
                 <div class="title">
                     <h2>Class 08</h2>
